8e354a9af9434889026d1aab705d5a0680c595fa
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-04-08 18:17:40 -0600

    update route handler

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
 	const cookieStore = cookies();
 	const token = cookieStore.get('token');
 
-	return new Response('Hello, Next.js!', {
+	return Response.json('Hello, Next.js!', {
 		status: 200,
 		headers: { 'Set-Cookie': `token=${token}` },
 	});
